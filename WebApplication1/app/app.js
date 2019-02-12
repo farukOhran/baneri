f540ccd778661d08d061f8ad083081f1015dd1e9
ohran <ohran@AZEM-PC>
2019-02-12 13:08:40 +0100

    komit

--- WebApplication1/app/app.js
+++ WebApplication1/app/app.js
@@ -1,67 +1,44 @@
 ﻿angular.module('app', [])
 
-    .controller('BeerCounter', function ($scope, $locale, $http, $q, $timeout, $interval) {
+    .controller('kontroler', function ($scope, $http, $timeout, $interval) {
 
 
-
-        $scope.banners = [];
-
+        
 
 
-        var _getBanners = function () {
-            var deferred = $q.defer();
+        function _getBanners() {
 
 
             $scope.testBanners = [];
 
 
-            $http.get('https://demo.evona.ba:3100/banners/0', { headers: { 'TerminalId': '2222' } }).then(function (response) {
+            $http({
+                method: 'GET', url: 'https://demo.evona.ba:3100/banners/0', headers: { 'TerminalId': '2222' }
+            }).then(function successCallback(response) {
+
+                var banner = response.data.Result;
+               
+                for (var i = 0; i < banner.length; i++) {
+
+                  
+                    $scope.testBanners.push(banner[i]);
+                }
 
 
-                _.each(response.data.Result, function (banner) {
-                    if (banner.ShowingType == 3 || banner.ShowingType == 5)
-                        banner.Visible = false;
-                        $scope.testBanners.push(banner);
-                });
+            }, function errorCallback(response) {
 
 
-                deferred.resolve($scope.testBanners);
-            }),
-                function (err, status) {
-                    deferred.reject(err);
-                };
 
-            return deferred.promise;
-        };
-
-
+            });
+        }
 
         _getBanners();
 
 
-        $scope.message = "Nesto";
+      
 
-        $timeout(function () {
+       
 
-            $scope.banners = $scope.testBanners;
-
-        }, 100);
-
-
-       $interval(function () {
-                var index = _.findIndex($scope.banners, function (banner) { return banner.Visible == true });
-                if (index == $scope.banners.length)
-                    index = 0;
-                if (index == -1) {
-                    $scope.banners[0].Visible = true;
-
-                }
-                else {
-                    $scope.banners[index].Visible = false;
-                    $scope.banners[index + 1].Visible = true;
-                }
-
-            }, 1000);
 
 
 
